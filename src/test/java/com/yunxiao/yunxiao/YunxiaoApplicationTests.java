839c71fa80cf52354ccd60e425e80eff64e29fb7
LuoYunXiao <1829462342@qq.com>
2023-09-10 10:27:30 +0800

    init

---
+++ src/test/java/com/yunxiao/yunxiao/YunxiaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yunxiao.yunxiao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YunxiaoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
